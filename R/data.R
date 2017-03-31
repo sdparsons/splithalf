294f1b139be32613ba04bc0a66491b4d22b901a4
Sam Parsons <sam.parsons@psy.ox.ac.uk>
2017-03-31 16:21:23 +0100

    modified data descriptions

--- R/data.R
+++ R/data.R
@@ -2,6 +2,15 @@
 #'
 #' A dataset containing data necessary to run examples of each function
 #'
+#' The following code was used to generate the data
+#'
+#' DPdata <- data.frame(subject = rep(1:20, each = (96*2)),
+#' blockcode = rep(c("block1","block2"), each = 96, length.out = 20*2*96),
+#' trialnum = rep(1:96, length.out = 20*2*96),
+#' congruency = rep(c("Congruent","Incongruent"), length.out = 20*2*96),
+#' latency = rep(rnorm(100,25), length.out = 20*2*96),
+#' correct = rep(1, length.out = 20*2*96))
+#'
 #' @format A dataframe with 3840 rows and 6 variables
 #' \itemize{
 #' \item subject: contains participant numbers for 20 subjects
@@ -14,6 +23,13 @@
 "DPdata"
 
 #' Generated dataset of Dot-probe data with missing data
+#'
+#' The data is adapted from the DPdata set using the following code
+#'
+#' DPdata_missing <- DPdata
+#' DPdata_missing$correct <- ifelse(DPdata_missing$subject == 15 &
+#'                                  DPdata_missing$blockcode == "block2",
+#'                                  0,1)
 #'
 #' A dataset containing data necessary to run examples of each function
 #' including missing data
