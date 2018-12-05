1749534a618aab054302e85ecee385c45586dfd1
Sam Parsons <sam.parsons@psy.ox.ac.uk>
2018-12-05 11:02:30 +0000

    warning correction

--- R/zzz.R
+++ R/zzz.R
@@ -9,5 +9,6 @@
   # packageStartupMessage(pkgname, " is BETA software! Please report any bugs.")
   packageStartupMessage("The (unofficial) version name is: Fight Milk")
   packageStartupMessage("For questions and issues, please see github.com/sdparsons/splithalf")
+  packageStartupMessage("There is full documentation at https://sdparsons.github.io/splithalf_documentation/")
   packageStartupMessage("or email sam.parsons@psy.ox.ac.uk")
 }
