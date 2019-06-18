03e96ca93bdfa877248fa24d4a83d65f1f2d81d9
sdparsons <sam.parsons@psy.ox.ac.uk>
2019-06-18 10:19:50 +0100

    fixed Rcpp components

--- src/Speedloop.cpp
+++ src/Speedloop.cpp
@@ -1,24 +1,16 @@
-#include <Rcpp.h>
+# include <Rcpp.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
 
 // [[Rcpp::export]]
 NumericMatrix Speedloop(NumericMatrix A, int x, NumericVector y){
   NumericVector tmp = y;
+  int l = y.size();
   NumericMatrix tmpM = A;
 
   for(int i = 0; i < x; i++) {
-    std::random_shuffle(tmp.begin(), tmp.end());
-    tmpM(_,i) = tmp;
+
+    tmpM(_,i) = Rcpp::sample(tmp, l);
   }
   return tmpM;
 
