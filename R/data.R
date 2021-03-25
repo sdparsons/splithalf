41a271b98002963cb8c51001e732203e8bf321c3
sdparsons <sam.parsons@psy.ox.ac.uk>
2021-03-25 17:37:56 +0000

    fixing data

--- R/data.R
+++ R/data.R
@@ -3,12 +3,12 @@
 #' This simulation was run to estimate the relative runtimes for different possible combinations of sample sizes and trial numbers etc.
 #'
 #' \itemize{
-#'   \item Sim codes for the simulation number
-#'   \item sample_size codes for the sample size
-#'   \item Number_of_conditions codes for the number of conditions run
-#'   \item trials codes for the number of trials
-#'   \item permutations codes for the number of permutations
-#'   \item runtime codes for the runtime in seconds
+#'   \item{Sim}{codes for the simulation number}
+#'   \item{sample_size}{codes for the sample size}
+#'   \item{Number_of_conditions}{codes for the number of conditions run}
+#'   \item{trials}{codes for the number of trials}
+#'   \item{permutations}{codes for the number of permutations}
+#'   \item{runtime}{codes for the runtime in seconds}
 #' }
 #'
 #' @docType data
