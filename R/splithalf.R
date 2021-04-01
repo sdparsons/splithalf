5ceef8d740e469a9b8eb185bef88fa4ff1183e08
sdparsons <sam.parsons@psy.ox.ac.uk>
2021-04-01 12:11:29 +0100

    minor corrections for validation

--- R/splithalf.R
+++ R/splithalf.R
@@ -536,7 +536,7 @@ splithalf <- function(data,
           n = dplyr::n()
         ) %>%
         dplyr::mutate(splithalf = round((cor1 + cor2) / 2, round.to)) %>%
-        dplyr::mutate(spearmanbrown = round((2 * splithalf) / ((1 + (2 - 1) * abs(splithalf)))), round.to) %>%
+        dplyr::mutate(spearmanbrown = round((2 * splithalf) / ((1 + (2 - 1) * abs(splithalf))), round.to)) %>%
         dplyr::select(-1, -2)
 
     }
